d7c0923cb14f4ca899c6baba054037410e49eccc
Baptiste TOULEMONDE <baptistetoul@gmail.com>
2020-02-11 12:11:20 +0100

    ajouter un titre #1

---
+++ src/app/todo-liste/todo-liste.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TodoListeComponent } from './todo-liste.component';
+
+describe('TodoListeComponent', () => {
+  let component: TodoListeComponent;
+  let fixture: ComponentFixture<TodoListeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TodoListeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodoListeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
